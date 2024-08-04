e925677ea5ccb32c281937e01c7e24e3a664db28
sasha <gitcommit@thebigsasha.com>
2024-08-04 00:25:58 -0700

    rust kv

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,60 @@
-use axum::{routing::get, Router};
-use tower_service::Service;
+use serde::{Deserialize, Serialize};
 use worker::*;
 
-fn router() -> Router {
-    Router::new().route("/", get(root))
-}
-
 #[event(fetch)]
-async fn fetch(
-    req: HttpRequest,
-    _env: Env,
-    _ctx: Context,
-) -> Result<axum::http::Response<axum::body::Body>> {
-    console_error_panic_hook::set_once();
-    Ok(router().call(req).await?)
-}
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    // Create an instance of the Router, which can use parameters (/user/:name) or wildcard values
+    // (/file/*pathname). Alternatively, use `Router::with_data(D)` and pass in arbitrary data for
+    // routes to access and share using the `ctx.data()` method.
+    let router = Router::new();
 
-pub async fn root() -> &'static str {
-    "Hello Axum!"
+    // useful for JSON APIs
+    #[derive(Deserialize, Serialize)]
+    struct Account {
+        id: u64,
+        // ...
+    }
+    router
+        .get_async("/account/:id", |_req, ctx| async move {
+            if let Some(id) = ctx.param("id") {
+                let accounts = ctx.kv("ACCOUNTS")?;
+                return match accounts.get(id).json::<Account>().await? {
+                    Some(account) => Response::from_json(&account),
+                    None => Response::error("Not found", 404),
+                };
+            }
+
+            Response::error("Bad Request", 400)
+        })
+        // handle files and fields from multipart/form-data requests
+        .post_async("/upload", |mut req, _ctx| async move {
+            let form = req.form_data().await?;
+            if let Some(entry) = form.get("file") {
+                match entry {
+                    FormEntry::File(file) => {
+                        let bytes = file.bytes().await?;
+                    }
+                    FormEntry::Field(_) => return Response::error("Bad Request", 400),
+                }
+                // ...
+
+                if let Some(permissions) = form.get("permissions") {
+                    // permissions == "a,b,c,d"
+                }
+                // or call `form.get_all("permissions")` if using multiple entries per field
+            }
+
+            Response::error("Bad Request", 400)
+        })
+        // read/write binary data
+        .post_async("/echo-bytes", |mut req, _ctx| async move {
+            let data = req.bytes().await?;
+            if data.len() < 1024 {
+                return Response::error("Bad Request", 400);
+            }
+
+            Response::from_bytes(data)
+        })
+        .run(req, env)
+        .await
 }
