9d6e44eb34da263960d7f1679ce5aceb26aee457
sasha <gitcommit@thebigsasha.com>
2024-08-04 01:40:04 -0700

    updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use worker::*;
 
+//TODO: https://github.com/cloudflare/workers-rs?tab=readme-ov-file#define-a-durable-object-in-rust
+
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
     // Create an instance of the Router, which can use parameters (/user/:name) or wildcard values
@@ -10,53 +12,25 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
 
     // useful for JSON APIs
     #[derive(Deserialize, Serialize)]
-    struct Account {
-        id: u64,
-        // ...
+    struct MetSeeItem {
+        name: String,
+        email: String,
+        url: String,
+        message: String,
+        eventID: String,
+        hasMet: bool,
+        code: String,
     }
     router
         .get_async("/", |_req, _ctx| async move {
             Response::ok("thebigsasha's 'met you there' / 'see you there' service API")
         })
-        .get_async("/account/:id", |_req, ctx| async move {
-            if let Some(id) = ctx.param("id") {
-                let accounts = ctx.kv("ACCOUNTS")?;
-                return match accounts.get(id).json::<Account>().await? {
-                    Some(account) => Response::from_json(&account),
-                    None => Response::error("Not found", 404),
-                };
-            }
-
-            Response::error("Bad Request", 400)
-        })
-        // handle files and fields from multipart/form-data requests
-        .post_async("/upload", |mut req, _ctx| async move {
-            let form = req.form_data().await?;
-            if let Some(entry) = form.get("file") {
-                match entry {
-                    FormEntry::File(file) => {
-                        let bytes = file.bytes().await?;
-                    }
-                    FormEntry::Field(_) => return Response::error("Bad Request", 400),
-                }
-                // ...
-
-                if let Some(permissions) = form.get("permissions") {
-                    // permissions == "a,b,c,d"
-                }
-                // or call `form.get_all("permissions")` if using multiple entries per field
-            }
-
-            Response::error("Bad Request", 400)
-        })
-        // read/write binary data
-        .post_async("/echo-bytes", |mut req, _ctx| async move {
-            let data = req.bytes().await?;
-            if data.len() < 1024 {
-                return Response::error("Bad Request", 400);
-            }
-
-            Response::from_bytes(data)
+        .post_async("/", |mut req, _ctx| async move {
+            let body = req.json::<MetSeeItem>().await?;
+            Response::ok(format!(
+                "Hello, {}! You have successfully posted a 'met you there' message to event {}",
+                body.name, body.eventID
+            ))
         })
         .run(req, env)
         .await
